1e1024d709efcf991bac9b3a89a94f9aa5f1aa44
Ketok <ketok4321@gmail.com>
2025-12-05 22:11:55 +0100

    Day 4 part 2

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -1,6 +1,6 @@
 use std::io::{self, BufRead};
 
-fn forkliftable(map: &Vec<String>, x: usize, y: usize, w: usize, h: usize) -> bool {
+fn forkliftable(map: &Vec<Vec<char>>, x: usize, y: usize, w: usize, h: usize) -> bool {
     let mut c = 0;
 
     for i in x.saturating_sub(1).max(0)..(x+2).min(h) {
@@ -8,7 +8,7 @@ fn forkliftable(map: &Vec<String>, x: usize, y: usize, w: usize, h: usize) -> bo
             if i == x && j == y {
                 continue;
             }
-            if map[i].chars().nth(j).unwrap() == '@' {
+            if map[i][j] == '@' {
                 c += 1;
                 if c == 4 {
                     return false;
@@ -20,14 +20,27 @@ fn forkliftable(map: &Vec<String>, x: usize, y: usize, w: usize, h: usize) -> bo
     true
 }
 
+fn remove(map: &mut Vec<Vec<char>>, w: usize, h: usize) -> usize {
+    let mut dr = 0;
+    for i in 0..h {
+        for j in 0..w {
+            if map[i][j] == '@' && forkliftable(&map, i, j, w, h) {
+                map[i][j] = '.';
+                dr += 1;
+            }
+        }
+    }
+    dr
+}
+
 fn main() {
     let mut r1 = 0;
     let mut r2 = 0;
 
-    let mut map: Vec<String> = vec![];
+    let mut map: Vec<Vec<char>> = vec![];
 
     for line in io::stdin().lock().lines().map(Result::unwrap) {
-        map.push(line);
+        map.push(line.chars().collect());
     }
 
     let w = map.first().unwrap().len();
@@ -35,9 +48,17 @@ fn main() {
 
     for i in 0..h {
         for j in 0..w {
-            if map[i].chars().nth(j).unwrap() == '@' && forkliftable(&map, i, j, w, h) {
+            if map[i][j] == '@' && forkliftable(&map, i, j, w, h) {
                 r1 += 1;
             }
+        }
+    }
+
+    loop {
+        let dr = remove(&mut map, w, h);
+        r2 += dr;
+        if dr == 0 {
+            break
         }
     }
 
